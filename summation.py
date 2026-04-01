fe328a832cbc49e562668630a645c74f03a89dd9
Joygit <joy.chakraborty.3520@gmail.com>
2026-04-02 00:34:16 +0530

    New pipeline testing code again

--- summation.py
+++ summation.py
@@ -3,12 +3,12 @@ numbers = [10, 20, 30, 40, 50]
 total = sum(numbers)
 final = total * 1.18
 # print(f"Total: {total}")
-# print(f"Final amount: {final}")
+# print(f"Final : {final}")
 
    
 
-github_output = os.environ.get("GITHUB_OUTPUT", "")
-if github_output:
-    with open(github_output, "a") as file:
-        file.write(f"total={total}\n")
-        file.write(f"final={final}\n")    
+# github_output = os.environ.get("GITHUB_OUTPUT", "")
+# if github_output:
+#     with open(github_output, "a") as file:
+#         file.write(f"total={total}\n")
+#         file.write(f"final={final}\n")    
